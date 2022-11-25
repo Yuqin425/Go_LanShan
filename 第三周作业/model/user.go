022896e5bc0b6d755d0330e1113775f5f715f746
Yuqin425 <114483458+Yuqin425@users.noreply.github.com>
2022-11-25 15:58:47 +0800

    第三周作业

---
+++ 第三周作业/model/user.go
@@ -0,0 +1,14 @@
+package model
+
+import "github.com/dgrijalva/jwt-go"
+
+type User struct {
+	Username  string `form:"username" json:"username" binding:"required"`
+	Password  string `form:"password" json:"password" binding:"required"`
+	telephone string `form:"telephone" json:"telephone" binding:"required"`
+}
+
+type MyClaims struct {
+	Username string
+	jwt.StandardClaims
+}
